5f2864343eb60a439307675e06120a6f6067c5ce
louisbl <louis@beltramo.me>
2015-02-02 00:46:23 +0100

    Add important lifecyle methods

--- app/src/main/java/crm/gobelins/lifecycle/MainActivity.java
+++ app/src/main/java/crm/gobelins/lifecycle/MainActivity.java
@@ -1,6 +1,33 @@
 package crm.gobelins.lifecycle;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
 }
